363d29f2e9e3125df475011593a4cbc607566592
Jerami Soriano <jerami4122@yahoo.com>
2024-02-23 18:06:35 -0500

    initial statistics page commit

--- app/(screens)/stats.js
+++ app/(screens)/stats.js
@@ -4,6 +4,7 @@ import { defaultStyles } from "../../constants/Styles";
 export default function Stats() {
   return (
     <View style={defaultStyles.container}>
+    <Text>Stats</Text>
       {/* <Text style={defaultStyles.title}>Stats</Text> */}
     </View>
   );
