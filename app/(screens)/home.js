3e4f38e812ccf4588398d7f0639f684e570637b9
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2024-02-28 02:51:12 -0500

    added mock entries

--- app/(screens)/home.js
+++ app/(screens)/home.js
@@ -47,15 +47,6 @@ export default function Home() {
     []
   );
 
-  // Mock Data: List of children
-  const data = useMemo(
-    () =>
-      Array(20)
-        .fill(0)
-        .map((_, index) => `Mock Data ${index + 1}`),
-    []
-  );
-
   const handleNewEntry = () => (
     router.navigate("/(modals)/new-entry"),
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
@@ -75,10 +66,37 @@ export default function Home() {
       {/* Previous Entries */}
       <View style={entryStyles.flatListContainer}>
         <FlatList
-          data={data}
+          data={Object.keys(mockEntries)}
           renderItem={({ item }) => (
-            <View style={entryStyles.prevEntryContainer}>
-              <Text style={entryStyles.title}>{item}</Text>
+            <View style={entryStyles.entryContainer}>
+              {/* Date */}
+              <View style={entryStyles.leftColumn}>
+                <View style={entryStyles.dateContainer}>
+                  <Text style={entryStyles.dayText}>
+                    {mockEntries[item].day}
+                  </Text>
+                  <Text style={entryStyles.dateText}>
+                    {mockEntries[item].date}
+                  </Text>
+                </View>
+              </View>
+              {/* Intensity */}
+              <View style={entryStyles.intensityContainer}>
+                <View style={entryStyles.intensityBoxContainer}>
+                  {Array.from({
+                    length: parseInt(mockEntries[item].intensity),
+                  }).map((_, index) => (
+                    <View key={index} style={entryStyles.intensityBox} />
+                  ))}
+
+                  {Array.from({
+                    length: 5 - parseInt(mockEntries[item].intensity),
+                  }).map((_, index) => (
+                    <View key={index} style={entryStyles.emptyBox} />
+                  ))}
+                </View>
+                <Text style={entryStyles.intensityText}>INTENSITY</Text>
+              </View>
             </View>
           )}
           keyExtractor={(item) => item}
@@ -98,7 +116,11 @@ export default function Home() {
         snapPoints={["50%"]}
         backdropComponent={renderBackdrop}
       >
-        <BottomSheetScrollView>{data.map(renderItem)}</BottomSheetScrollView>
+        <BottomSheetScrollView>
+          {Object.keys(mockChildren).map((key) =>
+            renderItem(mockChildren[key])
+          )}
+        </BottomSheetScrollView>
       </BottomSheetModal>
     </SafeAreaView>
   );
@@ -109,13 +131,15 @@ const entryStyles = StyleSheet.create({
     flex: 1,
     marginBottom: -34,
   },
-  prevEntryContainer: {
-    padding: 20,
+  entryContainer: {
+    padding: 10,
     marginVertical: 10,
     marginHorizontal: 20,
     borderColor: Colors.primary,
     borderRadius: 10,
     borderWidth: 2,
+    flexDirection: "row",
+    gap: 10,
   },
   title: {
     fontSize: 16,
@@ -123,9 +147,72 @@ const entryStyles = StyleSheet.create({
     color: "black",
   },
   text: {
-    fontSize: 24,
+    fontSize: 16,
     fontFamily: "DMSans",
-    color: "white",
+    color: "black",
+  },
+  textSmall: {
+    fontSize: 12,
+    fontFamily: "DMSans",
+    color: "black",
+  },
+
+  leftColumn: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    borderRadius: 4,
+    gap: 10,
+  },
+  dateContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 6,
+    gap: 4,
+    borderRadius: 4,
+    backgroundColor: "#eee",
+    width: "100%",
+  },
+  dayText: {
+    fontSize: 12,
+    fontFamily: "DMSans",
+    color: "black",
+  },
+  dateText: {
+    fontSize: 12,
+    fontFamily: "DMSans",
+    color: "black",
+  },
+
+  intensityContainer: {
+    flexDirection: "column",
+    padding: 2,
+    gap: 4,
+    borderRadius: 4,
+  },
+  intensityBoxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 2,
+  },
+  intensityBox: {
+    width: 15,
+    height: 10,
+    borderRadius: 2,
+    backgroundColor: Colors.tint,
+  },
+  emptyBox: {
+    width: 15,
+    height: 10,
+    borderRadius: 2,
+    borderWidth: 2,
+    borderColor: Colors.tint,
+  },
+  intensityText: {
+    fontSize: 10,
+    fontFamily: "DMMono",
+    color: "black",
   },
 });
 
@@ -176,3 +263,71 @@ const styles = StyleSheet.create({
     color: "black",
   },
 });
+
+const mockEntries = {
+  0: {
+    day: "WEDNESDAY",
+    date: "FEB-28-2024",
+    intensity: 2,
+  },
+  1: {
+    day: "TUESDAY",
+    date: "FEB-27-2024",
+    intensity: 1,
+  },
+  2: {
+    day: "MONDAY",
+    date: "FEB-26-2024",
+    intensity: 4,
+  },
+  3: {
+    day: "SUNDAY",
+    date: "FEB-25-2024",
+    intensity: 2,
+  },
+  4: {
+    day: "SATURDAY",
+    date: "FEB-24-2024",
+    intensity: 3,
+  },
+  5: {
+    day: "FRIDAY",
+    date: "FEB-23-2024",
+    intensity: 5,
+  },
+  6: {
+    day: "THURSDAY",
+    date: "FEB-22-2024",
+    intensity: 4,
+  },
+  7: {
+    day: "WEDNESDAY",
+    date: "FEB-21-2024",
+    intensity: 1,
+  },
+};
+
+// Mock Data: List of children
+const mockChildren = {
+  0: "John Doe",
+  1: "Jane Doe",
+  2: "John Smith",
+  3: "Jane Smith",
+  4: "John Johnson",
+  5: "Jane Johnson",
+  6: "John Brown",
+  7: "Jane Brown",
+  8: "John Davis",
+  9: "Jane Davis",
+  10: "John Miller",
+  11: "Jane Miller",
+  12: "John Wilson",
+  13: "Jane Wilson",
+  14: "John Moore",
+  15: "Jane Moore",
+  16: "John Taylor",
+  17: "Jane Taylor",
+  18: "John Anderson",
+  19: "Jane Anderson",
+  20: "John Thomas",
+};
